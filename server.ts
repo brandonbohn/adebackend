db3e7d647c7709a09fab6f160204fbd4e8ecf7c0
Brandon Bohn <brandonbohn@yahoo.com>
2025-12-13 16:33:32 +0300

    fix: remove duplicate imports and initialization from server.ts

--- server.ts
+++ server.ts
@@ -38,45 +38,7 @@ app.get('/api/check-json', (req: Request, res: Response) => {
     res.status(500).json({ error: 'Error reading adedata.json', details: err });
   }
 });
-import { Request, Response } from 'express';
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import contentRoutes from './routes/contentRoutes';
-import path from 'path';
-import { addDonor, getDonors, getDonorById, deleteDonor } from './controllers/donorController';
-import { createPaymentOption, updatePaymentOption, getPaymentOptions, deletePaymentOption } from './controllers/paymentOptionController';
-import { createCart, getCart, addItemToCart, removeItemFromCart } from './controllers/cartController';
-import { getAllAvailablePaymentOptions } from './controllers/paymentOptionController';
-import { processPayment } from './controllers/paymentOptionController';
-
-
-
-
 dotenv.config();
-console.log('Starting server...');
-const app = express();
-app.use(cors({
-	origin: 'https://www.adekiberafoundation.org',
-	credentials: true
-}));
-// Serve static JSON files
-
-app.use('/Backend/json', express.static(path.join(__dirname, 'json')));
-const port = process.env.PORT || 3000;
-
-
-app.use(express.json());
-// Connect to MongoDB
-const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/adefc';
-mongoose.connect(mongoUri)
-	.then(() => console.log('MongoDB connected'))
-	.catch((err: any) => console.error('MongoDB connection error:', err));
-
-// Root route
-interface RootRequest extends Request {}
-interface RootResponse extends Response {}
 
 app.get('/', (req: Request, res: Response) => {
 res.send('API is running');
